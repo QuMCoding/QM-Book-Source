5247ba20d1d06eeaabbec685853c8fd038910427
TWTom041 <s10855041@thjh.tp.edu.tw>
2023-07-10 23:08:10 +0800

    1

--- double_slits.py
+++ double_slits.py
@@ -8,15 +8,21 @@ lim_total = 1000
 hbar = 6.62607015e-34
 
 
-def new_shot(xes):
+def new_shot(xes, dots=lim_total):
     # a = lim_top - lim_bottom
     # p = xes
     # psi_p ** 2 = (a/(2*pi*hbar)) * (1+cos(p*a/hbar))
     a = lim_top - lim_bottom
     prob = (a / (2 * np.pi * hbar)) * (1. + np.cos(xes * a / hbar))
-    _x = np.random.choice(xes, lim_total, p=prob/np.sum(prob))
-    plt.scatter(_x, np.random.uniform(lim_bottom, lim_top, _x.shape), s=0.1)
+    _x = np.random.choice(xes, dots, p=prob/np.sum(prob))
+    plt.scatter(_x, np.random.uniform(lim_bottom, lim_top, _x.shape), s=0.1, c=np.zeros((len(_x),)))
 
 
-new_shot(np.linspace(lim_bottom, lim_top, 1000))
+new_shot(np.linspace(lim_bottom, lim_top, 1000), 10)
+plt.show()
+new_shot(np.linspace(lim_bottom, lim_top, 1000), 100)
+plt.show()
+new_shot(np.linspace(lim_bottom, lim_top, 1000), 1000)
+plt.show()
+new_shot(np.linspace(lim_bottom, lim_top, 1000), 10000)
 plt.show()
